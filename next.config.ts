c2d19bac8e05ae5cfc63370503ffa073a426a406
Quentin Maudry <quentin.maudry@studentfr.ch>
2026-03-31 14:32:30 +0200

    Remove basePath for custom domain

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Portfolio-cv",
-  assetPrefix: "/Portfolio-cv",
   images: { unoptimized: true },
 };
 
